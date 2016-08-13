cd0b971e91a1317232cd3312c93dbabe942d69bc
elier <tecnofel.e@gmail.com>
2016-08-13 03:03:24 +0200

    Prototipada Interfaz de Reservación con angularjs. Preparado el index.html para animaciones con angular. Creación de app.js como módulo para el proyecto. Creación de controlador de Provincias.

---
+++ js/controllerProv.js
@@ -0,0 +1,21 @@
+angular.module('Viajero', []).controller('controllerProvincias', function ($scope) {
+    $scope.provincia = [
+        {'nombre': 'Pinar del Río'},
+        {'nombre': 'Artemisa'},
+        {'nombre': 'La Habana'},
+        {'nombre': 'Mayabeque'},
+        {'nombre': 'Matanzas'},
+        {'nombre': 'Cienfuegos'},
+        {'nombre': 'Villa Clara'},
+        {'nombre': 'Sancti Spíritus'},
+        {'nombre': 'Ciego de Ávila'},
+        {'nombre': 'Camagüey'},
+        {'nombre': 'Las Tunas'},
+        {'nombre': 'Granma'},
+        {'nombre': 'Holguín'},
+        {'nombre': 'Santiago de Cuba'},
+        {'nombre': 'Guántanamo'},
+        {'nombre': 'Municipio Especial Isla de la Juventud'},
+        
+    ];
+});
